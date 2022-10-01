bf5273cfe5cef5d23c3296ca7059fc0bb83c1857
Exile10mg <boryn3it@gmail.com>
2022-10-01 20:24:25 +0200

    Updated

--- pages/urls.py
+++ pages/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('about', views.about, name="about"),
+    path('contact', views.contact, name="contact"),
+    path('services', views.services, name="services"),
+    path('cars', views.cars, name="cars"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'pages/home.html')
+
+def about(request):
+    return render(request, 'pages/about.html')
+
+def contact(request):
+    return render(request, 'pages/contact.html')
+
+def services(request):
+    return render(request, 'pages/services.html')
+
+def cars(request):
+    return render(request, 'pages/cars.html')
